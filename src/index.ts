3c53723ae69e74a9dcd124f23c0d5e186fb63d3a
Orion Gilliam <gilliamol@nih.gov>
2024-01-08 10:16:06 -0500

    import sucessful

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,66 @@
 import { ServiceSchema, ServiceSettingSchema, Service } from "moleculer";
-import * as Netsparker from 'netsparker-cloud';
-import { APIS, Authentication, HttpBasicAuth } from "netsparker-cloud";
+import * as Netsparker from "netsparker-cloud";
+import {
+  AccountLicenseApiModel,
+  Authentication,
+  HttpBasicAuth,
+} from "netsparker-cloud";
 
 // imported separately to make updates to APIDictionary easier
-import { AccountApi, AgentGroupsApi, AgentsApi, AuditLogsApi, AuthenticationProfilesApi, DiscoveryApi, IssuesApi, MembersApi, NotificationsApi, RolesApi, ScanPoliciesApi, ScanProfilesApi, ScansApi, TeamApi, TechnologiesApi, VulnerabilityApi, WebsiteGroupsApi, WebsitesApi } from "netsparker-cloud";
+import {
+  AccountApi,
+  AgentGroupsApi,
+  AgentsApi,
+  AuditLogsApi,
+  AuthenticationProfilesApi,
+  DiscoveryApi,
+  IssuesApi,
+  MembersApi,
+  NotificationsApi,
+  RolesApi,
+  ScanPoliciesApi,
+  ScanProfilesApi,
+  ScansApi,
+  TeamApi,
+  TechnologiesApi,
+  VulnerabilityApi,
+  WebsiteGroupsApi,
+  WebsitesApi,
+} from "netsparker-cloud";
 
 // re-export API client to expose all model types
-export * from 'netsparker-cloud';
+export * from "netsparker-cloud";
 
 export type NetsparkerAPIClient = {
   setDefaultAuthentication(authStrategy: Authentication): void;
-}
+};
 
 type APIDictionary = {
-  AccountApi: typeof AccountApi,
-  AgentGroupsApi: typeof AgentGroupsApi,
-  AgentsApi: typeof AgentsApi,
-  AuditLogsApi: typeof AuditLogsApi,
-  AuthenticationProfilesApi: typeof AuthenticationProfilesApi,
-  DiscoveryApi: typeof DiscoveryApi,
-  IssuesApi: typeof IssuesApi,
-  MembersApi: typeof MembersApi,
-  NotificationsApi: typeof NotificationsApi,
-  RolesApi: typeof RolesApi,
-  ScanPoliciesApi: typeof ScanPoliciesApi,
-  ScanProfilesApi: typeof ScanProfilesApi,
-  ScansApi: typeof ScansApi,
-  TeamApi: typeof TeamApi,
-  TechnologiesApi: typeof TechnologiesApi,
-  VulnerabilityApi: typeof VulnerabilityApi,
-  WebsiteGroupsApi: typeof WebsiteGroupsApi,
-  WebsitesApi: typeof WebsitesApi,
-}
+  AccountApi: typeof AccountApi;
+  AgentGroupsApi: typeof AgentGroupsApi;
+  AgentsApi: typeof AgentsApi;
+  AuditLogsApi: typeof AuditLogsApi;
+  AuthenticationProfilesApi: typeof AuthenticationProfilesApi;
+  DiscoveryApi: typeof DiscoveryApi;
+  IssuesApi: typeof IssuesApi;
+  MembersApi: typeof MembersApi;
+  NotificationsApi: typeof NotificationsApi;
+  RolesApi: typeof RolesApi;
+  ScanPoliciesApi: typeof ScanPoliciesApi;
+  ScanProfilesApi: typeof ScanProfilesApi;
+  ScansApi: typeof ScansApi;
+  TeamApi: typeof TeamApi;
+  TechnologiesApi: typeof TechnologiesApi;
+  VulnerabilityApi: typeof VulnerabilityApi;
+  WebsiteGroupsApi: typeof WebsiteGroupsApi;
+  WebsitesApi: typeof WebsitesApi;
+};
 
 type NetsparkerAPINames = keyof APIDictionary;
 
 export type NetsparkerAdapter = {
   [API in NetsparkerAPINames]: InstanceType<APIDictionary[API]>;
-}
+};
 
 export interface INetsparkerAdapterMixinSettings extends ServiceSettingSchema {
   /**
@@ -61,60 +84,66 @@ export interface INetsparkerAdapterMixinSettings extends ServiceSettingSchema {
   accountInfoOnStart?: boolean;
 }
 
-export const DefaultNetsparkerAdapterSettings: INetsparkerAdapterMixinSettings = {
-  netsparkerBasePath: undefined,
-  netsparkerUserId: undefined,
-  netsparkerToken: undefined,
-  accountInfoOnStart: true,
-};
+export const DefaultNetsparkerAdapterSettings: INetsparkerAdapterMixinSettings =
+  {
+    netsparkerBasePath: undefined,
+    netsparkerUserId: undefined,
+    netsparkerToken: undefined,
+    accountInfoOnStart: true,
+  };
 
-export interface INetsparkerAdapterMixin extends Service<INetsparkerAdapterMixinSettings> {
+export interface INetsparkerAdapterMixin
+  extends Service<INetsparkerAdapterMixinSettings> {
   netsparkerAdapter: NetsparkerAdapter;
   netsparkerAuth: HttpBasicAuth;
   netsparkerSDK: typeof Netsparker;
 }
 
-export const NetsparkerAdapterMixin: ServiceSchema<INetsparkerAdapterMixinSettings> = {
+export const NetsparkerAdapterMixin: ServiceSchema<INetsparkerAdapterMixinSettings> =
+  {
+    name: "Netsparker",
 
-  name: 'Netsparker',
+    settings: DefaultNetsparkerAdapterSettings,
 
-  settings: DefaultNetsparkerAdapterSettings,
+    metadata: {
+      netsparker: true,
+    },
 
-  metadata: {
-    netsparker: true,
-  },
+    actions: {},
 
-  actions: {},
+    methods: {},
 
-  methods: {},
+    created(this: INetsparkerAdapterMixin) {
+      this.netsparkerAdapter = {} as NetsparkerAdapter;
+      this.netsparkerAuth = new HttpBasicAuth();
+      this.netsparkerSDK = Netsparker;
+      if (!this.settings.netsparkerUserId) {
+        throw new Error("a value for netsparkerUserId was not provided!");
+      }
+      if (!this.settings.netsparkerToken) {
+        throw new Error("a value for netsparkerToken was not provided!");
+      }
+      this.netsparkerAuth.username = this.settings.netsparkerUserId;
+      this.netsparkerAuth.password = this.settings.netsparkerToken;
+      this.logger.info("Netsparker adapter: basic HTTP auth configured");
+      APIS.map((netsparkerAPI) => {
+        const APIName = netsparkerAPI.name as NetsparkerAPINames;
+        // @ts-ignore
+        this.netsparkerAdapter[APIName] = new netsparkerAPI(
+          this.settings.netsparkerBasePath
+        );
+        this.netsparkerAdapter[APIName].setDefaultAuthentication(
+          this.netsparkerAuth
+        );
+      });
+      this.logger.info("Netsparker adapter: enabled");
+    },
 
-  created(this: INetsparkerAdapterMixin) {
-    this.netsparkerAdapter = {} as NetsparkerAdapter;
-    this.netsparkerAuth = new HttpBasicAuth();
-    this.netsparkerSDK = Netsparker;
-    if (!this.settings.netsparkerUserId) {
-      throw new Error('a value for netsparkerUserId was not provided!')
-    }
-    if (!this.settings.netsparkerToken) {
-      throw new Error('a value for netsparkerToken was not provided!')
-    }
-    this.netsparkerAuth.username = this.settings.netsparkerUserId;
-    this.netsparkerAuth.password = this.settings.netsparkerToken;
-    this.logger.info('Netsparker adapter: basic HTTP auth configured');
-    APIS.map(netsparkerAPI => {
-      const APIName = netsparkerAPI.name as NetsparkerAPINames;
-      // @ts-ignore
-      this.netsparkerAdapter[APIName] = new netsparkerAPI(this.settings.netsparkerBasePath);
-      this.netsparkerAdapter[APIName].setDefaultAuthentication(this.netsparkerAuth);
-    });
-    this.logger.info('Netsparker adapter: enabled');
-  },
-
-  async started(this: INetsparkerAdapterMixin) {
-    if (this.settings.accountInfoOnStart) {
-      const { body: acccountDetails } = await this.netsparkerAdapter.AccountApi.accountLicense();
-      this.logger.info('Netsparker account info:', acccountDetails);
-    }
-  }
-
-};
+    async started(this: INetsparkerAdapterMixin) {
+      if (this.settings.accountInfoOnStart) {
+        const accountDetails: AccountLicenseApiModel =
+          await this.netsparkerAdapter.AccountApi.accountLicense();
+        this.logger.info("Netsparker account info:", accountDetails);
+      }
+    },
+  };
